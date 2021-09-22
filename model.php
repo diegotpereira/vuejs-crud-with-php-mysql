83d26225d1a7eae510b4e787ac9c9f5ae56e807c
Diego <diegoteixeirapereira@hotmail.com>
2021-09-22 15:24:12 -0300

    criação do arquivo de funções.

--- model.php
+++ model.php
@@ -4,7 +4,7 @@
         private $nomeUsuario = 'root';
         private $senha = 'root';
         private $nomeBanco = 'db_vuejs_crud_with_php_mysql';
-        private $conexao;
+        private $connection;
 
         // Criar Conexão
         public function __construct() 
@@ -12,6 +12,7 @@
             try {
                 //code...
                 $this->connection = new mysqli($this->host, $this->nomeUsuario, $this->senha, $this->nomeBanco);
+
             } catch (Exception $e) {
                 echo "Erro na Conexão" . $e->getMessage();
             }
@@ -21,7 +22,54 @@
         public function inserir($nome, $autor) {
             $query = "INSERT INTO tb_livro (livro_nome, livro_autor) VALUES ('".$nome."', '".$autor."')";
 
-            if ($sql = $this->conexao->query($query)) {
+            if ($sql = $this->connection->query($query)) {
+                # code...
+                return true;
+            } else {
+                # code...
+                return;
+            }
+            
+        }
+        public function buscarTodos() {
+            $data = [];
+            $query =  "SELECT * FROM tb_livro";
+            
+            if ($sql = $this->connection->query($query)) {
+                # code...
+                while($rows = mysqli_fetch_assoc($sql)) {
+                    $data[] = $rows;
+                }
+            }
+            return $data;
+        }
+        public function deletar($id) {
+            $query = "DELETE FROM tb_livro WHERE livro_id = '".$id."' ";
+
+            if ($sql = $this->connection->query($query)) {
+                # code...
+                return true;
+            } else {
+                # code...
+                return;
+            }
+            
+        }
+        public function editar($id) {
+            $data = [];
+            $query = "SELECT * FROM tb_livro WHERE livro_id = '".$id."' ";
+
+            if ($sql = $this->connection->query($query)) {
+                # code...
+                $row = mysqli_fetch_row($sql);
+                $data = $row;
+            }
+            return $data;
+        }
+        public function atualizar($id, $nome, $autor) {
+            $query = "UPDATE tb_livro SET livro_nome = '".$nome."', livro_autor = '".$autor."' WHERE livro_id = '".$id."' ";
+
+            if ($sql = $this->connection->query($query)) {
                 # code...
                 return true;
             } else {
